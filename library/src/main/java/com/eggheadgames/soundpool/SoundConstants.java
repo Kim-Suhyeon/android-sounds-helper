80e99fa9a533b1027c0ff92a8d2db8d86aba74e1
Alex Dibrivnyi <sashok.dib@gmail.com>
2016-05-30 01:04:33 +0300

    Added sounds. Added docs. Refactored

---
+++ library/src/main/java/com/eggheadgames/soundpool/SoundConstants.java
@@ -0,0 +1,11 @@
+package com.eggheadgames.soundpool;
+
+public class SoundConstants {
+    public static final int SOUND_CLEAR_SQUARE = 0;
+    public static final int SOUND_ENTER_LETTER = 1;
+    public static final int SOUND_UNDO = 2;
+    public static final int SOUND_ERROR = 3;
+    public static final int SOUND_SELECT_SQUARE = 4;
+    public static final int SOUND_SELECT_CATEGORY_OR_PUZZLE = 5;
+    public static final int SOUND_SUCCESS = 6;
+}
--- library/src/main/java/com/eggheadgames/soundpool/SoundUtil.java
+++ library/src/main/java/com/eggheadgames/soundpool/SoundUtil.java
@@ -15,43 +15,84 @@ public class SoundUtil {
 
     private static Map<Integer, Integer> cachedInitializedSounds = new HashMap<>();
 
-    public static void init(Activity context, boolean enabled) {
-        if (enabled) {
-            if (soundPool != null) {
-                return; //Already set up. Nothing more to do here.
-            }
+    /**
+     * Init sound pool
+     *
+     * @param activity
+     */
+    public static void init(Activity activity) {
+        if (soundPool != null) {
+            kill();
+        }
 
-            soundPool = new SoundPool(8, AudioManager.STREAM_MUSIC, 0);
-            context.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        add(SoundConstants.SOUND_CLEAR_SQUARE, R.raw.clear_square);
+        add(SoundConstants.SOUND_ENTER_LETTER, R.raw.enter_letter);
+        add(SoundConstants.SOUND_UNDO, R.raw.undo);
+        add(SoundConstants.SOUND_ERROR, R.raw.error);
+        add(SoundConstants.SOUND_SELECT_SQUARE, R.raw.select_square);
+        add(SoundConstants.SOUND_SELECT_CATEGORY_OR_PUZZLE, R.raw.select_category_or_puzzle);
+        add(SoundConstants.SOUND_SUCCESS, R.raw.success);
 
-            if (cachedInitializedSounds == null) {
-                cachedInitializedSounds = new HashMap<>();
-            }
+        soundPool = new SoundPool(8, AudioManager.STREAM_MUSIC, 0);
+        activity.setVolumeControlStream(AudioManager.STREAM_MUSIC);
 
-            for (Integer soundKey : predefinedSoundsKeys.keySet()) {
-                cachedInitializedSounds.put(soundKey, soundPool.load(context, predefinedSoundsKeys.get(soundKey), 1));
-            }
+        if (cachedInitializedSounds == null) {
+            cachedInitializedSounds = new HashMap<>();
+        }
 
-        } else {
-            if (soundPool != null) {
-                soundPool.release();
-                soundPool = null;
-            }
-
-            if (cachedInitializedSounds != null) {
-                cachedInitializedSounds.clear();
-                cachedInitializedSounds = null;
-            }
+        for (Integer soundKey : predefinedSoundsKeys.keySet()) {
+            cachedInitializedSounds.put(soundKey, soundPool.load(activity.getApplicationContext(),
+                    predefinedSoundsKeys.get(soundKey), 1));
         }
     }
 
+    /**
+     * Kill sound poll and clear cached sounds list.
+     */
+    public static void kill() {
+        if (soundPool != null) {
+            soundPool.release();
+            soundPool = null;
+        }
+
+        if (cachedInitializedSounds != null) {
+            cachedInitializedSounds.clear();
+            cachedInitializedSounds = null;
+        }
+    }
+
+    /**
+     * Toggle sound pool
+     *
+     * @param activity
+     * @param enable   - true - activate, false - deactivate
+     */
+    public static void toggle(Activity activity, boolean enable) {
+        if (enable) {
+            init(activity);
+        } else {
+            kill();
+        }
+    }
+
+    /**
+     * Function to add new sound to predefined list. Need to call init after new item or list of items was added.
+     *
+     * @param soundKey        - int id of sound. please do not use 0-6, they are predefined.
+     * @param soundResourceId - sound resource id
+     */
     public static void add(int soundKey, int soundResourceId) {
         if (!predefinedSoundsKeys.containsKey(soundKey)) {
             predefinedSoundsKeys.put(soundKey, soundResourceId);
         }
     }
 
-    //Play an already prepared sound sample
+    /**
+     * Play an already prepared sound sample
+     *
+     * @param context
+     * @param id      - sound id
+     */
     public static void playSound(Context context, int id) {
         if (soundPool == null || cachedInitializedSounds == null || !cachedInitializedSounds.containsKey(id)) {
             return;
@@ -64,6 +105,4 @@ public class SoundUtil {
 
         soundPool.play(cachedInitializedSounds.get(id), volume, volume, 1, 0, 1f);
     }
-
-
 }
