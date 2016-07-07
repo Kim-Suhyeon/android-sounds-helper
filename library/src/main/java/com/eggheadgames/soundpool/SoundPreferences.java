c7b5c49ea423068762fd2351dfdd7c330a06ac2f
Apisov <apisovp@gmail.com>
2016-07-07 17:06:15 +0300

    moved handling shared prefs into SoundUtil

--- library/src/main/java/com/eggheadgames/soundpool/SoundPreferences.java
+++ library/src/main/java/com/eggheadgames/soundpool/SoundPreferences.java
@@ -3,23 +3,22 @@ package com.eggheadgames.soundpool;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-public final class SoundPreferences {
+final class SoundPreferences {
 
     private static final String SOUND_UTIL_SHARED_PREFERENCES_NAME = "sound_util_shared_preferences";
     private static final String KEY_SOUND_STATE = "sound_state";
     private static final boolean DEFAULT_SOUND_STATE = true;
 
-    private SoundPreferences(){}
+    private SoundPreferences() {
+    }
 
-    public static void setSoundEnableState(Context context, boolean enabled) {
+    static void setSoundEnableState(Context context, boolean enabled) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(SOUND_UTIL_SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
         sharedPreferences.edit().putBoolean(KEY_SOUND_STATE, enabled).apply();
     }
 
-    public static boolean isSoundEnabled(Context context) {
+    static boolean isSoundEnabled(Context context) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(SOUND_UTIL_SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
         return sharedPreferences.getBoolean(KEY_SOUND_STATE, DEFAULT_SOUND_STATE);
     }
-
-
 }
